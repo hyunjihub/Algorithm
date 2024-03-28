f433d193a56e64c481f327400a1d39d94cbab6aa
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-03-28 13:29:03 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.js
@@ -0,0 +1,11 @@
+function solution(my_string, alp) {
+    var answer = '';
+    for(let i=0; i<my_string.length; i++) {
+        if(my_string.charAt(i)===alp) {
+            answer += my_string.charAt(i).toUpperCase();
+        } else {
+            answer += my_string.charAt(i);
+        }
+    }
+    return answer;
+}
