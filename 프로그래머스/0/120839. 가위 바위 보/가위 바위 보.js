467da93b75d90617ec04c2b9c4a20e8b20a4f455
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-03-28 13:03:14 +0900

    [level 0] Title: 가위 바위 보, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,13 @@
+function solution(rsp) {
+    var answer = '';
+    for(let i=0; i<rsp.length; i++) {
+        if(rsp.charAt(i)==='0') {
+            answer += '5';
+        } else if(rsp.charAt(i)==='2') {
+            answer += '0';
+        } else {
+            answer += '2';
+        }
+    }
+    return answer;
+}
