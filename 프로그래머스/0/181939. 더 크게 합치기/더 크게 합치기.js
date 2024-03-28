1e7342824b080b433130cb7ff07c00249e22f5e3
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-03-28 13:37:00 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    var answer = 0;
+    let ab = a.toString()+b.toString();
+    let ba = b.toString()+a.toString();
+    if(parseInt(ab)>=parseInt(ba)) {
+        answer = parseInt(ab);
+    } else {
+        answer = parseInt(ba);
+    }
+    return answer;
+}
