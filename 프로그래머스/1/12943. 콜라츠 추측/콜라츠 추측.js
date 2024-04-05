993d9a179862d2b0ab5513ebcbbf2399f1ac63f2
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-04-05 19:55:07 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,17 @@
+function solution(num) {
+    var answer = 0;
+    while(true) {
+        if(num===1) break;
+        if(answer>=500) {
+            answer = -1;
+            break;
+        }
+        if(num%2===0) {
+            num /= 2;
+        } else {
+            num = num*3+1;
+        }
+        answer++;
+    }
+    return answer;
+}
