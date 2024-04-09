17e4be7342bcf71117338fb276e1be8191cfe727
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-04-09 15:27:03 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 1.81 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.js
@@ -0,0 +1,50 @@
+function solution(numbers, hand) {
+    var answer = '';
+    let pad = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"], ["*", "0", "#"]]
+    let current = [[3, 0],[3, 2]];
+    let target = [0, 0];
+    let count = 0;
+    let max = 0;
+    
+    function useLeft() {
+        count = Math.abs(current[0][0]-target[0])+Math.abs(current[0][1]-target[1]);
+        current[0] = target;
+        answer += "L";
+    }
+
+    function useRight() {
+        count = Math.abs(current[1][0]-target[0])+Math.abs(current[1][1]-target[1]);
+        current[1] = target;
+        answer += "R";
+    }
+    
+    for(let i=0; i<numbers.length; i++) {
+        for(let j=0; j<pad.length; j++) {
+            for(let k=0; k<pad[0].length; k++) {
+                if(pad[j][k]===numbers[i].toString()) {
+                    target = [j, k];
+                    break;
+                }
+            }
+        }
+        if(numbers[i]===1 || numbers[i]===4 || numbers[i]===7) {
+            useLeft();
+        } else if(numbers[i]===3 || numbers[i]===6 || numbers[i]===9) {
+            useRight();
+        } else {
+            if(Math.abs(current[0][0]-target[0])+Math.abs(current[0][1]-target[1])>Math.abs(current[1][0]-target[0])+Math.abs(current[1][1]-target[1])) {
+                useRight();
+            } else if(Math.abs(current[0][0]-target[0])+Math.abs(current[0][1]-target[1])<Math.abs(current[1][0]-target[0])+Math.abs(current[1][1]-target[1])) {
+                useLeft();
+            } else {
+                if(hand==="right") {
+                    useRight();
+                } else {
+                    useLeft();
+                }
+            }
+        }
+    }
+    return answer;
+}
+
