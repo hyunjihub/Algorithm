e0d13f1ee8264b1332c58062325f9861e7819747
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-04-18 15:15:57 +0900

    [Silver III] Title: 이전 순열, Time: 200 ms, Memory: 10460 KB -BaekjoonHub

---
+++ 백준/Silver/10973. 이전 순열/이전 순열.js
@@ -0,0 +1,42 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let input = [];
+rl.on('line', function (line) {
+  input.push(line);
+})
+  .on('close', async function () {
+  // 답안 작성
+  let answer =0;    
+  let n = input[0];
+  let m = input[1];
+  answer = brute(n,m);
+  console.log(answer)
+  process.exit();
+});
+
+let brute = function(n,m){
+  let tmp = [];
+  let broke = -1;
+  let min = 10001;
+  m = m.split(' ');
+  for(let i=n-1;i>=0;i--){
+    if(min*1<m[i]*1){
+      broke = i;
+      break;
+    }
+    tmp.push(m[i]);
+    min = m[i]*1;
+  }  
+  if(broke == -1)return -1;
+  for(let i = 0;i<tmp.length;i++){
+    if(m[broke]*1>tmp[i]*1){
+      let change = tmp.splice(i,1,m[broke]);
+      m[broke] = change.join('');
+      break;
+    }
+  }  
+  return (m.slice(0,broke+1).join(' ') +' '+ tmp.join(' '));
+}
