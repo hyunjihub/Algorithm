94ab3d632cacbdeb0ebe95685e98bf6f50f00698
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-06-26 21:44:54 +0900

    [Silver V] Title: 야구 시즌, Time: 156 ms, Memory: 10512 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/21760. 야구 시즌/야구 시즌.js
+++ 백준/Silver/21760. 야구 시즌/야구 시즌.js
@@ -5,20 +5,22 @@ T = Number(T);
 
 let answer = '';
 for (let i = 0; i < T; i++) {
-  let max = 0;
   let [N, M, k, D] = input[i].trim().split(' ').map(Number);
-  for (let A = 1; ; A++) {
-    let B = A / k;
-    if (B % 1 !== 0) continue;
 
-    let inGame = N * (M - 1) * (A / 2) * M;
-    let outGame = (N - 1) * M * N * M * (B / 2);
-    let allGame = inGame + outGame;
-    if (allGame > D) break;
-    else if (D - max > D - allGame) max = allGame;
+  let numerator = 2 * D;
+  let denominator = N * (M * (M - 1) * k + (N - 1) * M * M);
+
+  let B = Math.floor(numerator / denominator);
+
+  if (B < 1) {
+    answer += '-1\n';
+  } else {
+    let A = k * B;
+    let inGame = N * M * (M - 1) * (A / 2);
+    let outGame = N * M * (N - 1) * M * (B / 2);
+    let totalGame = inGame + outGame;
+    answer += totalGame + '\n';
   }
-  if (max === 0) answer += '-1' + '\n';
-  else answer += max + '\n';
 }
 
 console.log(answer.trim());
