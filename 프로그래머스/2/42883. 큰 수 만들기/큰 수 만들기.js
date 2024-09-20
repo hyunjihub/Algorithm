281a5981af18489645ce5dee216e7022844472c4
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-09-20 21:47:57 +0900

    [level 2] Title: 큰 수 만들기, Time: 30.91 ms, Memory: 49.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,14 +1,4 @@
 function solution(number, k) {
-    let stack = [];
-    for (let i = 0; i < number.length; i++) {
-        let current = number[i];
-        while (stack.length > 0 && k > 0 && stack[stack.length - 1] < current) {
-            stack.pop();
-            k--;
-        }
-        stack.push(current);
-    }
-
-    stack = k > 0 ? stack.slice(0, stack.length - k) : stack;
-    return stack.join("");
+    var answer = '';
+    return answer;
 }
