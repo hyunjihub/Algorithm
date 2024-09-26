ab664cae9c9f526b2a7c15c57d267a1a1a4e4f95
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-09-26 19:33:58 +0900

    [Silver III] Title: 주유소, Time: 260 ms, Memory: 38636 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -2,20 +2,20 @@ const fs = require('fs');
 let [N, length, price] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 
 N = Number(N);
-length = length.trim().split(' ').map(Number);
-price = price.trim().split(' ').map(Number);
+length = length.trim().split(' ').map(BigInt);
+price = price.trim().split(' ').map(BigInt);
 
-let minPrice = 0;
+let minPrice = 0n;
 for (let i = 0; i < N - 1; i++) {
   let nextGas = i;
-  let move = 0;
+  let move = 0n;
   while (true) {
     if (nextGas === N - 1 || price[i] > price[nextGas]) break;
     nextGas++;
-    move += length[nextGas - 1];
+    move = move + length[nextGas - 1];
   }
   minPrice += price[i] * move;
   i = nextGas - 1;
 }
 
-console.log(minPrice);
+console.log(minPrice.toString());
