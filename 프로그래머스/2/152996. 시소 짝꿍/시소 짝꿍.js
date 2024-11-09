5aa7a9b76111acc0650e2b7d6ca5afcd79447f4b
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2024-11-09 18:49:16 +0900

    [level 2] Title: 시소 짝꿍, Time: 201.92 ms, Memory: 37.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -0,0 +1,13 @@
+function solution(weights) {
+    let answer = 0;
+    const map = new Map();
+    const ratio = [1, 3 / 2, 2, 4 / 3];
+    weights.sort((a, b) => b - a);
+    
+    weights.forEach(w => {
+        ratio.forEach(r => {if (map.has(w * r)) answer += map.get(w * r)});
+        map.set(w, (map.get(w) || 0) + 1);
+    });
+
+    return answer;
+}
