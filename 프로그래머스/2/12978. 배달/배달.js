b585686b228ddaef5b56d8e87162d3a8bdf611ae
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2025-02-02 19:42:17 +0900

    [level 2] Title: 배달, Time: 13.02 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.js
+++ 프로그래머스/2/12978. 배달/배달.js
@@ -1,30 +1,27 @@
 function solution(N, road, K) {
-    let graph = Array.from({ length: N + 1 }, () => []);
-    for (let i = 0; i < road.length; i++) {
-        graph[road[i][0]].push([road[i][1], road[i][2]]);
-        graph[road[i][1]].push([road[i][0], road[i][2]]);
+    let graph = Array.from({length: N+1}, ()=>[]);
+    for(let i=0; i<road.length; i++) {
+        let [a, b, c] = road[i];
+        graph[a].push([b, c]);
+        graph[b].push([a, c]);
     }
-
-    let distance = Array.from({ length: N + 1 }, () => Infinity);
-    let queue = [[0, 1]];
+    
+    let distance = Array.from({length: N+1}, ()=>Infinity);
     distance[1] = 0;
+    let queue = [[0, 1]];
+    
+    while(queue.length) {
+        queue.sort((a, b)=>a[0]-b[0]);
+        let [dist, current] = queue.shift();
 
-    while (queue.length > 0) {
-        queue.sort((a, b) => a[0] - b[0]);
-        let [dist, to] = queue.shift();
-
-        for (let i = 0; i < graph[to].length; i++) {
-            let [nextNode, cost] = graph[to][i];
-            let newDist = dist + cost;
-            
-            if (distance[nextNode] > newDist) {
-                distance[nextNode] = newDist;
-                queue.push([newDist, nextNode]);
+        for(let i=0; i<graph[current].length; i++) {
+            let [next, cost] = graph[current][i];
+            if(dist+cost<distance[next]) {
+                distance[next] = dist+cost;
+                queue.push([dist+cost, next]);
             }
         }
     }
-
-    let answer = distance.filter(d => d <= K).length;
-
-    return answer;
+    
+    return distance.filter((i)=>i<=K).length;
 }
