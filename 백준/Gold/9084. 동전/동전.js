2190ae34cacab448b5fd45ba0435baf9e958d456
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2025-02-17 18:47:52 +0900

    [Gold V] Title: 동전, Time: 168 ms, Memory: 10548 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.js
+++ 백준/Gold/9084. 동전/동전.js
@@ -3,17 +3,23 @@ let [T, ...input] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 
 T = Number(T);
 let index = 0;
+
+let answer = '';
 for (let i = 0; i < T; i++) {
   let N = Number(input[index++]);
-  let coin = input[index++].trim().split(' ').map(Number);
+  let coins = input[index++].trim().split(' ').map(Number);
   let M = Number(input[index++]);
 
   let dp = Array.from({ length: M + 1 }, () => 0);
   dp[0] = 1;
-  for (let i = 0; i < N; i++) {
-    for (let j = 0; j <= M; j++) {
-      if (j - coin[i] >= 0) dp[j] += dp[j - coin[i]];
+
+  for (let j = 0; j < coins.length; j++) {
+    for (let k = coins[j]; k <= M; k++) {
+      dp[k] += dp[k - coins[j]];
     }
   }
-  console.log(dp[M]);
+
+  answer += dp[M] + '\n';
 }
+
+console.log(answer.trim());
