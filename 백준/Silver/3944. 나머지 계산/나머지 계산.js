4c1cad8611b08e9e84d0d2729e0affaeaea19bbd
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2025-02-25 14:11:22 +0900

    [Silver II] Title: 나머지 계산, Time: 404 ms, Memory: 74336 KB -BaekjoonHub

--- 백준/Silver/3944. 나머지 계산/나머지 계산.js
+++ 백준/Silver/3944. 나머지 계산/나머지 계산.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
-let [T, ...n] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+let [T, ...input] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 
+T = Number(T);
 let answer = '';
 for (let i = 0; i < T; i++) {
+  let [B, D] = input[i].trim().split(' ');
+  B = Number(B);
+
   let sum = 0;
-  let [B, D] = n[i].trim().split(' ');
   for (let j = 0; j < D.length; j++) {
     sum += Number(D[j]);
   }
-  answer += (sum % (Number(B) - 1)) + '\n';
+
+  answer += (sum % (B - 1)) + '\n';
 }
 
 console.log(answer.trim());
