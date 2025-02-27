fa8742965e09b745d6c075169a774c15ca0cb16b
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2025-02-27 13:08:58 +0900

    [Silver III] Title: 개미, Time: 124 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Silver/10158. 개미/개미.js
+++ 백준/Silver/10158. 개미/개미.js
@@ -1,24 +1,24 @@
 const fs = require('fs');
-let [n, m, t] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+let [size, start, t] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 
-let [w, h] = n.trim().split(' ').map(Number);
-let [p, q] = m.trim().split(' ').map(Number);
+let [w, h] = size.trim().split(' ').map(Number);
+let [p, q] = start.trim().split(' ').map(Number);
 t = Number(t);
 
-p += t;
-q += t;
+let endP = p + t;
+let endQ = q + t;
 
-let xCycle = 2 * w;
-let yCycle = 2 * h;
-
-let xPosition = p % xCycle;
-if (xPosition > w) {
-  xPosition = xCycle - xPosition;
+let answer = [0, 0];
+if (Math.floor(endP / w) % 2 === 0) {
+  answer[0] = endP % w;
+} else {
+  answer[0] = w - (endP % w);
 }
 
-let yPosition = q % yCycle;
-if (yPosition > h) {
-  yPosition = yCycle - yPosition;
+if (Math.floor(endQ / h) % 2 === 0) {
+  answer[1] = endQ % h;
+} else {
+  answer[1] = h - (endQ % h);
 }
 
-console.log(xPosition, yPosition);
+console.log(answer.join(' '));
