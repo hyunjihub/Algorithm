72c3b4dea5d67f48c4d35b8865101a3a8872ed4a
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2025-03-05 12:25:34 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 100 ms, Memory: 11864 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.js
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.js
@@ -1,15 +1,14 @@
-var fs = require('fs');
-let [N, ...floats] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
+const fs = require('fs');
+let [N, ...num] = fs.readFileSync(0, 'utf-8').toString().trim().split('\n');
 
 N = Number(N);
-let dp = Array.from({ length: N }, () => 1);
-dp[0] = Number(floats[0]);
-let max = dp[0];
+num = num.map((i) => Number(i.trim()));
+
+let dp = Array.from({ length: N }, () => 0);
+dp[0] = num[0];
 
 for (let i = 1; i < N; i++) {
-  floats[i] = Number(floats[i]);
-  dp[i] = Math.max(floats[i], dp[i - 1] * floats[i]);
-  max = Math.max(max, dp[i]);
+  dp[i] = Math.max(dp[i - 1] * num[i], num[i]);
 }
 
-console.log(max.toFixed(3));
+console.log(Math.max(...dp).toFixed(3));
