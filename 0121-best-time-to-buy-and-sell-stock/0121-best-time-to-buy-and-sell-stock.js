a7f57f282a77a11d3a10d8a6f4a716d7232c3f8d
hyunjihub <97017935+hyunjihub@users.noreply.github.com>
2025-04-02 21:51:58 +0900

    Time: 5 ms (26.83%), Space: 65.2 MB (33.98%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let max = 0;
+    let min = prices[0];
+
+    for(let i=1; i<prices.length; i++) {
+        min = Math.min(min, prices[i]);
+        max = Math.max(max, prices[i]-min);
+    }
+
+    return max;
+};
